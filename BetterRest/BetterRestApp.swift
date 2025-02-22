92a8bf2dbe06b3c1335f36c3338a733ae3c54a4d
GreenRamSilgen <kiranshrs@yahoo.com>
2025-02-21 17:04:16 -0800

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Kiran Shrestha on 2/21/25.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
